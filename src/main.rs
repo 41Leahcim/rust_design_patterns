b82f4c1086dc20251aa2aca5cdf27bdc704426a7
Michael Scholten <michael-scholten@hotmail.nl>
2024-01-14 13:29:48 +0100

    concatenating strings

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,24 @@
-fn good_three_vowels(word: &str) -> bool {
-    let mut vowel_count = 0;
-    for c in word.chars() {
-        match c {
-            'a' | 'e' | 'i' | 'o' | 'u' => {
-                vowel_count += 1;
-                if vowel_count >= 3 {
-                    return true;
-                }
-            }
-            _ => vowel_count = 0,
-        }
-    }
-    false
+use std::fmt::Write as _;
+
+pub fn long_say_hello(name: &str) -> String {
+    let mut result = "Hello ".to_owned();
+    result.push_str(name);
+    result.push('!');
+    result
 }
 
-#[allow(clippy::ptr_arg)]
-fn bad_three_vowels(word: &String) -> bool {
-    good_three_vowels(word)
+pub fn format_say_hello(name: &str) -> String {
+    format!("Hello {name}!")
+}
+
+pub fn write_say_hello(name: &str) -> String {
+    let mut buffer = String::with_capacity(name.len());
+    write!(&mut buffer, "Hello {name}!").unwrap();
+    buffer
 }
 
 fn main() {
-    let ferris = "Ferris";
-    let curious = "Curious";
-    println!("{ferris}: {}", bad_three_vowels(&ferris.to_string()));
-    println!("{curious}: {}", bad_three_vowels(&curious.to_owned()));
-
-    // Would fail
-    //println!("{ferris}: {}", bad_three_vowels(ferris));
-    //println!("{curious}: {}", bad_three_vowels(curious));
-
-    println!("{ferris}: {}", good_three_vowels(ferris));
-    println!("{curious}: {}", good_three_vowels(curious));
-
-    let sentence = "Once upon a time, there was a friendly curious crab named Ferris".to_string();
-    for word in sentence.split_whitespace() {
-        if good_three_vowels(word) {
-            println!("{word} has three consecutive vowels!");
-        }
-    }
+    println!("{}", long_say_hello("Amy"));
+    println!("{}", format_say_hello("Amy"));
+    println!("{}", write_say_hello("Amy"));
 }
